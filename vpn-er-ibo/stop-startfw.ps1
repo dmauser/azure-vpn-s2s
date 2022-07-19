519042a70a2a7ec776ae12bd8a7ab179344148e6
Daniel Mauser <dmauser@hotmail.com>
2022-07-19 12:18:01 -0500

    updates

--- vpn-er-ibo/stop-startfw.ps1
+++ vpn-er-ibo/stop-startfw.ps1
@@ -6,7 +6,7 @@ $RG = "lab-er-vpn-s2s-Internet"
 #Stop Firewall
 
 $azfw=Get-AzFirewall -ResourceGroupName $RG
-$azfw | ForEach-Object -Parallel {
+$azfw | ForEach-Object {
     $_.Deallocate() 
     Write-Host "Stopping Azure Firewall" $_.name
     Set-AzFirewall -AzureFirewall $_ | Out-Null
@@ -15,12 +15,21 @@ $azfw | ForEach-Object -Parallel {
 
 #Start Firewall
 
+
+
 $azfw=Get-AzFirewall -ResourceGroupName $RG
 $azfw | ForEach-Object -Parallel {
-    $publicip = Get-AzPublicIpAddress -ResourceGroupName $RG -Name ($_.name + '-pip')
-    $vnet = Get-AzVirtualNetwork -name ($_.name).trim("-azfw") -ResourceGroupName $RG
+    $publicip = Get-AzPublicIpAddress -ResourceGroupName $RG -Name Az-Hub-azfw-pip
+    $vnet = Get-AzVirtualNetwork -Name Az-Hub-vnet -ResourceGroupName $RG
     $_.Allocate($vnet,$publicip) 
     Write-Host "Starting Azure Firewall" $_.name
     Set-AzFirewall -AzureFirewall $_ | Out-Null
     Write-Host "Azure Firewall" $_.name "has started"
 }
+
+
+$azfw = Get-AzFirewall -Name "Az-Hub-azfw" -ResourceGroupName $RG
+$publicip = Get-AzPublicIpAddress -ResourceGroupName $RG -Name Az-Hub-azfw-pip
+$vnet = Get-AzVirtualNetwork -Name Az-Hub-vnet -ResourceGroupName $RG
+$azfw.Allocate($vnet,$publicip)
+Set-AzFirewall -AzureFirewall $azfw
